5f7140f65def35fa7ebf39311e092e386564e915
bella <maekawatakaya10.8@gmail.com>
2025-03-17 11:23:52 +0900

    edit for pkg outputter bash file

--- .global/output_pkg_list.sh
+++ .global/output_pkg_list.sh
@@ -1,5 +1,8 @@
-pacman -Qeq > pacman_packages.txt
+#!/bin/bash
 
-cd ~/.cache/yay/
-find . -mindepth 2 -maxdepth 2 -type d -printf '%P\n' | sed 's/\/.*//' | sort > yay_packages.txt
-mv yay_packages.txt ~/bash/
+cd "$(dirname "$0")"
+mkdir -p pkg
+pacman -Qeq > pkg/pacman_pkgs.txt
+
+find ~/.cache/yay/ -mindepth 2 -maxdepth 2 -type d -printf '%P\n' | sed 's/\/.*//' | sort > pkg/yay_pkgs.txt
+ 
