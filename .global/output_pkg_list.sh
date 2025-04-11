d1787869a0b1ee86c9e425c55f422d40bbf9787a
bella <maekawatakaya10.8@gmail.com>
2025-04-12 00:28:47 +0900

    move and update

--- .global/output_pkg_list.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$0")"
-mkdir -p pkg
-pacman -Qeq >pkg/pacman_pkgs.txt
-
-find ~/.cache/yay/ -mindepth 2 -maxdepth 2 -type d -printf '%P\n' | sed 's/\/.*//' | sort >pkg/yay_pkgs.txt
