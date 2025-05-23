50aef8f7d5bb88ebb51ad20d9d09c60839216144
verazza <me@verazza.dev>
2025-05-23 13:42:31 +0900

    add $PROFILE

---
+++ .win/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,9 @@
+# $HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
+# $PROFILE
+
+Invoke-Expression (&starship init powershell)
+
+if (Get-Command lazygit -ErrorAction SilentlyContinue) {
+  Set-Alias -Name lg -Value lazygit
+  # Write-Host "Alias 'lg' for 'lazygit' created."
+}
