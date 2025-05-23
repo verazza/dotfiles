92de8881a56d288d560a6ec13915ec5b230761a9
verazza <me@verazza.dev>
2025-05-23 19:18:51 +0900

    del php-cs-fixer setting

--- .win/Microsoft.PowerShell_profile.ps1
+++ .win/Microsoft.PowerShell_profile.ps1
@@ -11,12 +11,6 @@ if (Get-Command lazygit -ErrorAction SilentlyContinue) {
   # Write-Host "Alias 'lg' for 'lazygit' created."
 }
 
-# php-cs-fixerが存在する場合、環境変数を設定
-if (Get-Command php-cs-fixer -ErrorAction SilentlyContinue) {
-    $env:PHP_CS_FIXER_IGNORE_ENV = "1"
-    # Write-Host "Environment variable 'PHP_CS_FIXER_IGNORE_ENV' set to 1 because php-cs-fixer was found."
-}
-
 if (Get-Command nvim -ErrorAction SilentlyContinue) {
   function n {
     nvim ./
