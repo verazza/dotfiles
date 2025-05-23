cf76e5880cab8011228d3a74c947d9a222c33289
verazza <me@verazza.dev>
2025-05-23 14:39:28 +0900

    add exported env variable

--- .win/Microsoft.PowerShell_profile.ps1
+++ .win/Microsoft.PowerShell_profile.ps1
@@ -10,3 +10,9 @@ if (Get-Command lazygit -ErrorAction SilentlyContinue) {
   Set-Alias -Name lg -Value lazygit
   # Write-Host "Alias 'lg' for 'lazygit' created."
 }
+
+# php-cs-fixerが存在する場合、環境変数を設定
+if (Get-Command php-cs-fixer -ErrorAction SilentlyContinue) {
+    $env:PHP_CS_FIXER_IGNORE_ENV = "1"
+    # Write-Host "Environment variable 'PHP_CS_FIXER_IGNORE_ENV' set to 1 because php-cs-fixer was found."
+}
